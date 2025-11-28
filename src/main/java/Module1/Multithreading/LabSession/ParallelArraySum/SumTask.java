9cb9b879586f94811133a6bce6adc4b38c5f45cb
CkavdiaRI <chirag.kavdia_1@scaler.com>
2025-11-28 07:16:18 +0530

    Lab Session on Synchronization - Q1

---
+++ src/main/java/Module1/Multithreading/LabSession/ParallelArraySum/SumTask.java
@@ -0,0 +1,22 @@
+package Module1.Multithreading.LabSession.ParallelArraySum;
+import java.util.concurrent.Callable;
+
+public class SumTask implements Callable<Long> {
+    private int[] array;
+    private int start, end;
+
+    public SumTask(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public Long call() {
+        long sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
